82b5edef8fac837316594dae76d3710148ecb7db
veloii <85405932+veloii@users.noreply.github.com>
2025-01-28 20:22:17 +0000

    refactor

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Veloi.';
-export const SITE_DESCRIPTION = ' Crafting open-source solutions to simplify & enhance the developer experience. ';
+export const SITE_TITLE = "Veloi.";
+export const SITE_DESCRIPTION =
+	" Crafting open-source solutions to simplify & enhance the developer experience. ";
